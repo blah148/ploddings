2af74fa08c14682d1fcac9f7c8b09a01c70abbe0
blah148 <info@ploddings.com>
2024-01-27 15:06:51 -0700

    modified db.js for supabase

--- db.js
+++ db.js
@@ -2,11 +2,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ploddings',
-  password: 'password',
-  port: 5432,
+  user: 'postgres.bmvuqgfxczoytjwjpvcn',
+  host: 'aws-0-us-west-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'm6H!DI!qV@lfLRL$',
+  port: 6543,  // The default PostgreSQL port, can be different based on your Supabase configuration
+  ssl: {
+    rejectUnauthorized: false,  // This may be needed for some cloud providers like Heroku or Supabase
+  }
 });
 
 module.exports = {
