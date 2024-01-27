5cf0472a12b39fb2f0e87f443ead6bc18c9f14b6
blah148 <info@ploddings.com>
2024-01-27 14:33:48 -0700

    modified threads to be similar to songs dynamic route

--- pages/threads/[id].js
+++ pages/threads/[id].js
@@ -1,47 +1,41 @@
-// pages/threads/[id].js
+// Access to: pathname, query, asPath, route
 import { useRouter } from 'next/router';
-// Import your database utility if you have one
+// Centralized location to globally manage database queries/operations
+const { fetchSlugsFromTable, fetchDataBySlug } = require('../../db-utilities');
 
 export default function Thread({ threadData }) {
+  // Initializing router object, containing info about current route
   const router = useRouter();
+  // Destructures the "id" parameter from the router.query property      
   const { id } = router.query;
 
+  // Conditional rendering while there's fetching from the db about dynamic id
   if (router.isFallback) {
     return <div>Loading...</div>;
-  }
-
+  } 
+   
   return (
     <div>
-      <h1>{threadData.title}</h1>
-      <p>{threadData.content}</p>
+      <h1>{threadData.thread_name}</h1>
+      <p>{threadData.meta_description}</p>
       {/* Render other thread details */}
     </div>
   );
-}
+} 
 
 export async function getStaticPaths() {
   // Fetch the list of slugs from your threads table
-  const slugs = await fetchSlugsFromThreadsTable();
+  const slugs = await fetchSlugsFromTable('threads');
   const paths = slugs.map(slug => ({
     params: { id: slug },
   }));
-
+   
   return { paths, fallback: true };
-}
+} 
 
 export async function getStaticProps({ params }) {
   // Fetch the thread data based on the slug
-  const threadData = await fetchThreadData(params.id);
+  const threadData = await fetchDataBySlug('threads', params.id);
   return { props: { threadData } };
-}
-
-async function fetchSlugsFromThreadsTable() {
-  // Implement the logic to fetch slugs from your threads table
-  // This might be a database query
-}
-
-async function fetchThreadData(slug) {
-  // Implement the logic to fetch a thread's data based on its slug
-  // This might be a database query
-}
+} 
 
