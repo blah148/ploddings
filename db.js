d1030f51f00806d61f3b2de50428123284a87c1d
blah148 <info@ploddings.com>
2024-01-27 16:01:14 -0700

    adding local env vars for dynamic production/development usage

--- db.js
+++ db.js
@@ -2,14 +2,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres.bmvuqgfxczoytjwjpvcn',
-  host: 'aws-0-us-west-1.pooler.supabase.com',
-  database: 'postgres',
-  password: 'm6H!DI!qV@lfLRL$',
-  port: 6543,  // The default PostgreSQL port, can be different based on your Supabase configuration
-  ssl: {
-    rejectUnauthorized: false,  // This may be needed for some cloud providers like Heroku or Supabase
-  }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
