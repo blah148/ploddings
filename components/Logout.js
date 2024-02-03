ea220fe3bd0b80cc2d3e93150257aae8518cc82a
blah148 <info@ploddings.com>
2024-02-03 08:25:43 -0700

    logout works in conjunction with login, using server-side api

--- components/Logout.js
+++ components/Logout.js
@@ -3,14 +3,10 @@ import { useRouter } from 'next/router';
 export default function Logout() {
     const router = useRouter();
 
-    const handleLogout = () => {
-        // Remove the JWT token cookie
-        // Here, we are setting the cookie to expire immediately
-        document.cookie = "auth_token=; Max-Age=0; path=/;";
-
-        // Redirect to the home page
-        router.push('/');
-    };
+		const handleLogout = async () => {
+			await fetch('/api/logout', { method: 'POST' });
+			router.push('/');
+		};
 
     return (
         <div>
