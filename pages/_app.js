b98fe5841a547327205d3484b615f0a7856861f5
blah148 <info@ploddings.com>
2024-02-05 16:01:01 -0700

    light vs dark mode now affects all pages on front-end via _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
+// Import your dependencies
 import "@/styles/globals.css";
 import { AuthProvider } from '../context/AuthContext';
-import { ThemeProvider } from '../context/ThemeContext';
+import { ThemeProvider, useTheme } from '../context/ThemeContext';
+
+// This is a new component that consumes the useTheme hook
+const ThemedAppContent = ({ Component, pageProps }) => {
+  const { theme } = useTheme();
+
+  return (
+    <div className={`app ${theme}`}>
+      <Component {...pageProps} />
+    </div>
+  );
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-	  	<ThemeProvider>
-        <Component {...pageProps} />
-  		</ThemeProvider>
+      <ThemeProvider>
+        {/* Use the ThemedAppContent component to wrap your page content */}
+        <ThemedAppContent Component={Component} pageProps={pageProps} />
+      </ThemeProvider>
     </AuthProvider>
   );
 }
