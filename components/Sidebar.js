d173b8f873c94ca23a0735837133df58c7f1c690
blah148 <info@ploddings.com>
2024-02-06 19:08:08 -0700

    linked the home button on the sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -33,6 +33,7 @@ export default function Sidebar({ userId, isAuthenticated, ip }) {
 
 	return (
 		<div>
+		<Link href="/" legacyBehavior>
 			<a>
 				<svg
 					role="img"
@@ -42,12 +43,13 @@ export default function Sidebar({ userId, isAuthenticated, ip }) {
 					className="Svg-sc-ytk21e-0 haNxPq home-active-icon"
 					viewBox="0 0 24 24"
 					data-encore-id="icon"
-				>
-					<path style={{ fill: 'white' }} d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732 V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1 .732l-7.5-4.33z">
+				> 
+					<path style={{ fill: 'white' }} d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732 V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z">
 					</path>
 				</svg>
 				<div>Home</div>
 			</a>
+		</Link>
 			<div>
 				<h2>Visit History</h2>
 				<ul>
