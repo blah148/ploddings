a6f0318d975e385c36e540076d9d86b0156a2c0f
blah148 <info@ploddings.com>
2024-02-18 12:05:32 -0700

    added fallback for blogs without parent

--- components/ParentBackLink.js
+++ components/ParentBackLink.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Link from 'next/link'; // Import Link from Next.js for navigation
 
-export default function IpodMenuLink({ threadData }) {
+export default function IpodMenuLink({ threadData, fallBack }) {
+  // Determine the href dynamically: use threadData if available, otherwise fallBack
+  const href = threadData && threadData.page_type && threadData.slug
+               ? `/${threadData.page_type}/${threadData.slug}`
+               : `/${fallBack}`;
+
   return (
-    <Link href={`/${threadData.page_type}/${threadData.slug}`} passHref> 
-      <div className="previous-button ">
+    <Link href={href} passHref>
+      <div className="previous-button">
         <svg role="img" height="auto" width="auto" aria-hidden="true" className="Svg-sc-ytk21e-0 leya-dW IYDlXmBmmUKHveMzIPCF" viewBox="0 0 16 16" data-encore-id="icon" style={{ marginBottom: 0 }}>
           <path fill="currentColor" d="M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06 1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z"></path>
         </svg>
--- components/ParentInfoLink.js
+++ components/ParentInfoLink.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Link from 'next/link'; // Import Link from Next.js for navigation
 
-export default function ParentInfoLink({ threadData }) {
-  return ( 
-    <Link href={`/${threadData.page_type}/${threadData.slug}`} passHref>
-      <img loading="lazy" alt={threadData.featured_img_alt_text} src={threadData.thumbnail_200x200} />
-      <div>{threadData.name}</div>
+export default function ParentInfoLink({ threadData, fallBack }) {
+  // Check if threadData exists and has the necessary properties, otherwise use fallBack
+  const href = threadData && threadData.page_type && threadData.slug
+               ? `/${threadData.page_type}/${threadData.slug}`
+               : `/${fallBack}`;
+
+  return (
+    <Link href={href} passHref>
+        <img loading="lazy" alt={threadData?.featured_img_alt_text || 'Default Alt Text'} src={threadData?.thumbnail_200x200 || '/default-thumbnail.png'} />
+        <div>{threadData?.name || 'Default Name'}</div>
     </Link>
   );
 }
