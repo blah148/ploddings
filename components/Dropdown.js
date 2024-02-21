aa28b9d04ecb8a1b667865b550cd1fe352abcfa0
blah148 <info@ploddings.com>
2024-02-21 15:42:16 -0700

    fixing up the chapters

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { supabase } from '../pages/utils/supabase'; // Adjust the import path as necessary
 import styles from '../styles/songs.module.css';
 import DownChevron from './DownChevron';
-import RelatedContent from './RelatedGrid_Songs';
+import Link from 'next/link'; // Make sure to import the Link component
 
 function Dropdown({ id }) {
   const [isVisible, setIsVisible] = useState(false);
+  const [relatedContents, setRelatedContents] = useState([]);
   const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const fetchRelatedContents = async () => {
+      let { data, error } = await supabase
+        .from('junction_related_content')
+        .select('content_id2, content:content_id2 (id, page_type, slug, pagination_title)')
+        .eq('content_id1', id);
+
+      if (error) {
+        console.error('Error fetching related content:', error.message);
+      } else {
+        setRelatedContents(data.map(item => item.content));
+      }
+    };
+
+    if (id) {
+      fetchRelatedContents();
+    }
+  }, [id]);
 
   const toggleDropdown = () => setIsVisible(!isVisible);
 
@@ -28,14 +49,24 @@ function Dropdown({ id }) {
   return (
     <div ref={dropdownRef}>
       <button 
-				onClick={toggleDropdown} 
-				className={styles.dropdownButton}>
-				<label>Chapters</label>
-				<DownChevron />
-			</button>
-      {isVisible && <div className={styles.dropdownContainer}>
-			  <RelatedContent id={id} />	
-			</div>}
+        onClick={toggleDropdown} 
+        className={styles.dropdownButton}>
+        <label>Chapters</label>
+        <DownChevron />
+      </button>
+      {isVisible && (
+        <div className={styles.dropdownContainer}>
+          {relatedContents.length > 0 ? (
+            relatedContents.map((content, index) => (
+              <Link href={`/${content.page_type}/${content.slug}`} key={content.id} passHref>
+                <a className={styles.linkStyle}>{content.pagination_title}</a> {/* Use a styled <a> tag within Link */}
+              </Link>
+            ))
+          ) : (
+            <p>No related content found.</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
