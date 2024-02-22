0c49c66d1347c162f7dd1cdab3a9ec6fca125bf6
blah148 <info@ploddings.com>
2024-02-21 17:24:30 -0700

    setup wikipedia and disco links

--- pages/threads/[id].js
+++ pages/threads/[id].js
@@ -10,6 +10,12 @@ import jwt from 'jsonwebtoken';
 const { fetchThreadData } = require('../../db-utilities');
 import { useLoading } from '../../context/LoadingContext';
 import Loader from '../../components/Loader';
+import styles from '../../styles/songs.module.css';
+import Footer from '../../components/Footer';
+import Menu from '../../components/Menu';
+import IpodMenuLink from '../../components/ParentBackLink';
+import VictrolaIcon from '../../components/VictrolaIcon';
+import WikipediaIcon from '../../components/WikipediaIcon';
 
 const verifyUserSession = (req) => {
   const token = req.cookies['auth_token'];
@@ -47,14 +53,33 @@ export default function Thread({ userId, ip, threadData }) {
   return (
     <div className="bodyA">
 			<Sidebar userId={userId} ip={ip} />
-			<div className="mainFeed">
-				<Loader isLoading={isLoading} />
-				<h1>{threadData.name}</h1>
-				<FavoriteButton userId={userId} id={threadData.id} ip={ip} />
-				<ChatWithGPT initialPrompt={`who is ${threadData.name}`} />
-				<div>{threadData.id}</div>
-				<img src={threadData.link_3 || 'https://f005.backblazeb2.com/file/ploddings-threads/featured_img_550px/default_550px.webp'} />
-				<TableDataFetcher threadId={threadData.id} />
+			<div className="mainFeedAll">
+				<div className="feedContainer">
+					<Loader isLoading={isLoading} />
+			    <div className="mainFeed">
+						<div className="topRow">
+							<IpodMenuLink fallBack='/' />
+							<Menu userId={userId} />
+						</div>
+					  <div className={styles.headerContainer}>		
+							<img src={threadData.link_3 || 'https://f005.backblazeb2.com/file/ploddings-threads/featured_img_550px/default_550px.webp'} />
+							<div className={styles.rightColumn}>
+								<div className={styles.songNameContainer}>
+									<h1>{threadData.name}</h1>
+									<FavoriteButton userId={userId} id={threadData.id} ip={ip} />
+								</div>
+								{threadData.body_text && (<div className={styles.lifeAndDeath}>{threadData.body_text}</div>)}
+								<div className={styles.iconContainer}>
+									{threadData.link_1 && (<WikipediaIcon link={threadData.link_1} />)}
+									{threadData.link_2 && (<VictrolaIcon link={threadData.link_2} />)}
+								</div>
+								<ChatWithGPT initialPrompt={`who is ${threadData.name}`} />
+							</div>
+						</div>
+						<TableDataFetcher threadId={threadData.id} />
+					</div>
+				</div>
+				<Footer userId={userId} />
 			</div>
     </div>
   );
