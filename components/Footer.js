8f376d71591ba9666693dcdea7ee5eca7bca4bce
blah148 <info@ploddings.com>
2024-02-26 11:12:16 -0700

    added youtube slow downer generic

--- components/Footer.js
+++ components/Footer.js
@@ -9,40 +9,41 @@ export default function Footer ({ userId = null  }) {
   };
 
 
-return (
-  <div className="footerContainer">
-    <div className="footer_higher-row">
-      <div className="footer_column">
-        <h2 className="footer_header">Usage</h2>
-        {!userId && (
-          <>
-            <Link href="/create-account" className="footer_link-Item" passHref>Create account</Link>
-            <Link href="/login" className="footer_link-item" passHref>Login</Link>
-          </>
-        )}
-        {userId && (
-          <>
-            <Link href="/account" className="footer_link-item" passHref>My account</Link>
-            <a onClick={handleLogout} className="footer_link-item">Logout</a>
-          </>
-        )}
-      </div>
-      <div className="footer_column">
-        <h2 className="footer_header">Open source</h2> 
-        <a href="https://github.com/blah148/ploddings" rel="noopener noreferrer" className="footer_link-item">Github</a>
-        <a href="https://github.com/blah148/ploddings/pulls" rel="noopener noreferrer" className="footer_link-item">Contribute</a>
-      </div>
-      <div className="footer_column">
-        <h2 className="footer_header">Feedback</h2>
-        <a href="https://github.com/blah148/ploddings/issues" rel="noopener noreferrer" className="footer_link-item">Report a bug</a>
-      </div>
-    </div>
-    <div className="footer_lower-row">
-      <Link href="/privacy-policy" className="footer_lower-item" passHref>Privacy policy</Link>
-      <Link href="/terms-and-conditions" className="footer_lower-item" passHref>Terms & conditions</Link>
-    </div>
-  </div>
-);
+	return (
+		<div className="footerContainer">
+			<div className="footer_higher-row">
+				<div className="footer_column">
+					<h2 className="footer_header">Content</h2> 
+					<Link href="/" className="footer_link-item" passHref>Home</Link>
+					<Link href="/slow-downer" className="footer_link-item" passHref>Slow-downer</Link>
+				</div>
+				<div className="footer_column">
+					<h2>Usage</h2>
+					{!userId && (
+						<>
+							<Link href="/create-account" className="footer_link-Item" passHref>Create account</Link>
+							<Link href="/login" className="footer_link-item" passHref>Login</Link>
+						</>
+					)}
+					{userId && (
+						<>
+							<Link href="/account" className="footer_link-item" passHref>My account</Link>
+							<a onClick={handleLogout} className="footer_link-item">Logout</a>
+						</>
+					)}
+				</div>
+				<div className="footer_column">
+					<h2 className="footer_header">Github</h2> 
+					<a href="https://github.com/blah148/ploddings" rel="noopener noreferrer" className="footer_link-item">Create a fork</a>
+					<a href="https://github.com/blah148/ploddings/issues" rel="noopener noreferrer" className="footer_link-item">Report a bug</a>
+				</div>
+			</div>
+			<div className="footer_lower-row">
+				<Link href="/privacy-policy" className="footer_lower-item" passHref>Privacy policy</Link>
+				<Link href="/terms-and-conditions" className="footer_lower-item" passHref>Terms & conditions</Link>
+			</div>
+		</div>
+	);
 
 }
 
--- pages/api/youtube-dl.js
+++ pages/api/youtube-dl.js
@@ -1,7 +1,34 @@
 import { join } from 'path';
-import { createWriteStream } from 'fs';
+import { existsSync, unlink } from 'fs';
 import ytdl from 'ytdl-core';
 import ffmpeg from 'fluent-ffmpeg';
+
+// Store scheduled deletion timers
+const deletionTimers = new Map();
+
+const deleteAfter = 43200000; // 12 hours in milliseconds
+
+// Function to schedule or reset file deletion
+function scheduleFileDeletion(filePath) {
+    // Clear existing timer if present
+    if (deletionTimers.has(filePath)) {
+        const timer = deletionTimers.get(filePath);
+        clearTimeout(timer);
+        deletionTimers.delete(filePath);
+    }
+
+    // Schedule new deletion timer
+    const timer = setTimeout(() => {
+        unlink(filePath, (err) => {
+            if (err) console.error(`Failed to delete file: ${filePath}`, err);
+            else console.log(`File deleted: ${filePath}`);
+        });
+        deletionTimers.delete(filePath);
+    }, deleteAfter);
+
+    // Store the timer
+    deletionTimers.set(filePath, timer);
+}
 
 export default async (req, res) => {
   if (req.method !== 'POST') {
@@ -19,26 +46,37 @@ export default async (req, res) => {
     const info = await ytdl.getInfo(videoID);
     const title = info.videoDetails.title.replace(/[^\w\s]/gi, '').replace(/\s+/g, '_');
     const outputPath = join(process.cwd(), 'public', 'tmp', `${title}.mp3`);
+		let fileUrl = `http://${req.headers.host}/tmp/${title}.mp3`;
 
-    const videoStream = ytdl(videoUrl, { quality: 'highestaudio' });
-    ffmpeg(videoStream)
-      .audioCodec('libmp3lame')
-      .format('mp3')
-      .on('error', (err) => {
-        console.error('An error occurred:', err.message);
-        res.status(500).end('Conversion failed due to an error.');
-      })
-      .on('end', () => {
-        // Construct the URL to access the saved MP3 file
-        const fileUrl = `http://${req.headers.host}/tmp/${title}.mp3`;
-        console.log(`File saved: ${outputPath}`);
-        res.status(200).json({ message: 'Conversion successful', url: fileUrl });
-      })
-      .save(outputPath);
+    // Check if the file already exists
+    if (existsSync(outputPath)) {
+      console.log(`File already exists: ${outputPath}`);
+      // Extend the file's lifetime
+      scheduleFileDeletion(outputPath);
+			return res.status(200).json({ message: 'File served from existing conversion.', url: fileUrl });
+    } else {
+      const videoStream = ytdl(videoUrl, { quality: 'highestaudio' });
+      ffmpeg(videoStream)
+        .audioCodec('libmp3lame')
+        .format('mp3')
+        .save(outputPath)
+        .on('end', () => {
+          console.log(`File saved: ${outputPath}`);
+          // Schedule the file for deletion in 12 hours
+          scheduleFileDeletion(outputPath);
+        })
+        .on('error', (err) => {
+          console.error('An error occurred:', err.message);
+          res.status(500).end('Conversion failed due to an error.');
+        });
+    }
+
+    // Construct the URL to access the saved MP3 file
+    fileUrl = `http://${req.headers.host}/tmp/${title}.mp3`;
+    res.status(200).json({ message: 'Conversion successful', url: fileUrl });
 
   } catch (error) {
     console.error('Conversion failed:', error);
-    // Ensure a response is sent even if an error occurs outside of ffmpeg's error handler
     if (!res.headersSent) {
       res.status(500).json({ error: 'Internal server error.' });
     }
