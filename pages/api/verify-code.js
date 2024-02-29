a822750050c9b3fe4266ca93d217434604550209
blah148 <info@ploddings.com>
2024-02-29 11:45:08 -0700

    more tweaks to verify-code api

--- pages/api/verify-code.js
+++ pages/api/verify-code.js
@@ -1,10 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from '../../utils/supabase';
 require('dotenv').config({ path: '../../.env' });
 import jwt from 'jsonwebtoken';
 import { serialize } from 'cookie';
-
-// Initialize Supabase client
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
