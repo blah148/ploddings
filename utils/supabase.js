11887e3c88f855631b3d53cf7cde89d578206c0b
blah148 <info@ploddings.com>
2024-02-28 23:24:36 -0700

    made necessary changes

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 require('dotenv').config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // const supabaseUrl2 = process.env.NEXT_PUBLIC_SUPABASE_URL;
 // const supabaseKey2 = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
