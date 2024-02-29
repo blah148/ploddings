5c1e1e68065b368870bdba092ab1a0948b436300
blah148 <info@ploddings.com>
2024-02-29 00:03:51 -0700

    made necessary changes

--- utils/supabase.js
+++ utils/supabase.js
@@ -6,7 +6,5 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // const supabaseUrl2 = process.env.NEXT_PUBLIC_SUPABASE_URL;
 // const supabaseKey2 = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
- console.log(`this should be the supabaseUrl: ${supabaseUrl}`);
- console.log(`this should be the supabaseKey: ${supabaseKey}`);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
