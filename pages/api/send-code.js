2e7fa9c2c2146640f2ce59570e4c08e05c27fe0c
blah148 <info@ploddings.com>
2024-02-29 11:52:57 -0700

    fixed api for send-code

--- pages/api/send-code.js
+++ pages/api/send-code.js
@@ -1,11 +1,8 @@
 import sgMail from '@sendgrid/mail';
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from '../../utils/supabase';
 require('dotenv').config({ path: '../../.env' });
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// Initialize Supabase client
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
