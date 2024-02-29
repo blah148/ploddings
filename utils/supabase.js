95aeb6b4276432a907250ec60afaaf62e5a14533
blah148 <info@ploddings.com>
2024-02-28 23:04:13 -0700

    cleanup for yarn build

--- utils/supabase.js
+++ utils/supabase.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabaseUrl2 = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey2 = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-// console.log(`this should be the var2: ${supabaseUrl2}`);
-// console.log(`this should be the var2: ${supabaseKey2}`);
+// const supabaseUrl2 = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseKey2 = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+ console.log(`this should be the supabaseUrl: ${supabaseUrl}`);
+ console.log(`this should be the supabaseKey: ${supabaseKey}`);
 
-export const supabase = createClient(supabaseUrl2, supabaseKey2);
+export const supabase = createClient(supabaseUrl, supabaseKey);
