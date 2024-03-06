69272efcb4d4f6d34b07f48e800d7f5aa559801f
blah148 <info@ploddings.com>
2024-03-06 14:58:28 -0700

    added creation alert to account creation on account page

--- components/Footer.js
+++ components/Footer.js
@@ -25,7 +25,7 @@ export default function Footer ({ userId = null  }) {
 					<h2>Usage</h2>
 					{!userId && (
 						<>
-							<Link href="/account" className="footer_link-Item" passHref>Account</Link>
+							<Link href="/account" className="footer_link-Item" passHref>Create account</Link>
 							<Link href="/login" className="footer_link-item" passHref>Login</Link>
 						</>
 					)}
