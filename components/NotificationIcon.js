b0c7ea5398eb70d96da189009033021c2531caa9
blah148 <info@ploddings.com>
2024-04-07 22:08:39 -0600

    cleaning up notifications more

--- components/NotificationIcon.js
+++ components/NotificationIcon.js
@@ -1,6 +1,7 @@
 import { supabase } from '../utils/supabase';
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
+import DeleteNotification from './DeleteNotification';
 
 const NotificationIcon = ({ userId = null }) => {
   const [unreadNotificationsCount, setUnreadNotificationsCount] = useState(0);
@@ -94,25 +95,61 @@ const NotificationIcon = ({ userId = null }) => {
     };
   }, [showMenu]);
 
-  const formatTimeAgo = (timestamp) => {
-    const now = new Date();
-    const notificationDate = new Date(timestamp);
-    const diffInSeconds = Math.floor((now - notificationDate) / 1000);
-    const minute = 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const week = day * 7;
-    const month = day * 30;
-    const year = day * 365;
+const convertToUTCFormat = (timestamp) => {
+  // Create a Date object from the timestamp string and enforce UTC by appending 'Z'
+  const dateObject = new Date(timestamp + 'Z');
+  
+  // Return the Date object, not the string
+  return dateObject;
+};
 
-    if (diffInSeconds < minute) return `${diffInSeconds}s ago`;
-    else if (diffInSeconds < hour) return `${Math.floor(diffInSeconds / minute)}m ago`;
-    else if (diffInSeconds < day) return `${Math.floor(diffInSeconds / hour)}h ago`;
-    else if (diffInSeconds < week) return `${Math.floor(diffInSeconds / day)}d ago`;
-    else if (diffInSeconds < month) return `${Math.floor(diffInSeconds / week)}w ago`;
-    else if (diffInSeconds < year) return `${Math.floor(diffInSeconds / month)}mo ago`;
-    else return `${Math.floor(diffInSeconds / year)}y ago`;
-  };
+const formatTimeAgo = (timestamp) => {
+  // Create a date object for the current time in UTC
+  const now = new Date();
+ 
+  // Convert the notification timestamp to a date object and adjust to UTC
+	const notificationDate = convertToUTCFormat(timestamp);
+
+  // Calculate the difference in seconds between the current time and the notification time
+  const diffInSeconds = Math.floor((now - notificationDate) / 1000);
+  
+  
+  const minute = 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const week = day * 7;
+  const month = day * 30;
+  const year = day * 365;
+
+  // Use the calculated difference to determine the time ago string
+  if (diffInSeconds < minute) return `${diffInSeconds}s ago`;
+  else if (diffInSeconds < hour) return `${Math.floor(diffInSeconds / minute)}m ago`;
+  else if (diffInSeconds < day) return `${Math.floor(diffInSeconds / hour)}h ago`;
+  else if (diffInSeconds < week) return `${Math.floor(diffInSeconds / day)}d ago`;
+  else if (diffInSeconds < month) return `${Math.floor(diffInSeconds / week)}w ago`;
+  else if (diffInSeconds < year) return `${Math.floor(diffInSeconds / month)}mo ago`;
+  else return `${Math.floor(diffInSeconds / year)}y ago`;
+};
+
+const deleteNotification = async (notificationId) => {
+    const { error } = await supabase
+        .from('notifications')
+        .delete()
+        .match({ id: notificationId });
+
+    if (error) {
+        console.error('Error deleting notification:', error.message);
+    } else {
+        // Immediately update UI by filtering out the deleted notification
+        setNotifications(currentNotifications =>
+            currentNotifications.filter(notification => notification.id !== notificationId)
+        );
+
+        // Optionally, adjust unread notification count if necessary
+        // Assuming you have a function to decrement the unread count
+        // decrementUnreadNotificationCount();
+    }
+};
 
 	function truncateHtmlText(htmlContent, maxLength) {
 		const parser = new DOMParser();
@@ -139,50 +176,50 @@ const NotificationIcon = ({ userId = null }) => {
 
   return (
 
-	<>
-		{notifications.length > 0 && (
-			<>
-				<svg onClick={toggleMenu} className="notIcon" viewBox="0 0 36 36">
-					<defs>
-						<style>
-							{`.cls-1 { fill: none; }`}
-						</style>
-					</defs>
-					<path fill="#c3c7c7" transform="scale(0.85)" d="M28.7071,19.293,26,16.5859V13a10.0136,10.0136,0,0,0-9-9.9492V1H15V3.0508A10.0136,10.0136,0,0,0,6,13v3.5859L3.2929,19.293A1,1,0,0,0,3,20v3a1,1,0,0,0,1,1h7v.7768a5.152,5.152,0,0,0,4.5,5.1987A5.0057,5.0057,0,0,0,21,25V24h7a1,1,0,0,0,1-1V20A1,1,0,0,0,28.7071,19.293ZM19,25a3,3,0,0,1-6,0V24h6Zm8-3H5V20.4141L7.707,17.707A1,1,0,0,0,8,17V13a8,8,0,0,1,16,0v4a1,1,0,0,0,.293.707L27,20.4141Z"/>
-					{unreadNotificationsCount > 0 && (
-						<>
-							<circle cx="24" cy="8" r="9.5" fill="red" />
-							<text x="24" y="10" fontSize="17" fontWeight="700" fill="white" textAnchor="middle" dominantBaseline="middle">
-								{unreadNotificationsCount}
-							</text>
-						</>
-					)}
-					<rect fill="#c3c7c7" transform="scale(0.85)" id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" className="cls-1" width="32" height="32"/>
-				</svg>
+		<>
+			<svg onClick={toggleMenu} className="notIcon" viewBox="0 0 36 36">
+				<defs>
+					<style>
+						{`.cls-1 { fill: none; }`}
+					</style>
+				</defs>
+				<path fill="#c3c7c7" transform="scale(0.85)" d="M28.7071,19.293,26,16.5859V13a10.0136,10.0136,0,0,0-9-9.9492V1H15V3.0508A10.0136,10.0136,0,0,0,6,13v3.5859L3.2929,19.293A1,1,0,0,0,3,20v3a1,1,0,0,0,1,1h7v.7768a5.152,5.152,0,0,0,4.5,5.1987A5.0057,5.0057,0,0,0,21,25V24h7a1,1,0,0,0,1-1V20A1,1,0,0,0,28.7071,19.293ZM19,25a3,3,0,0,1-6,0V24h6Zm8-3H5V20.4141L7.707,17.707A1,1,0,0,0,8,17V13a8,8,0,0,1,16,0v4a1,1,0,0,0,.293.707L27,20.4141Z"/>
+				{unreadNotificationsCount > 0 && (
+					<>
+						<circle cx="24" cy="8" r="9.5" fill="red" />
+						<text x="24" y="10" fontSize="17" fontWeight="700" fill="white" textAnchor="middle" dominantBaseline="middle">
+							{unreadNotificationsCount}
+						</text>
+					</>
+				)}
+				<rect fill="#c3c7c7" transform="scale(0.85)" id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" className="cls-1" width="32" height="32"/>
+			</svg>
 
-				<div ref={menuRef} className={`menu ${showMenu ? 'show' : ''}`}>
-						{notifications.map((notification) => (
-							<div key={notification.id} className="notItem">
-								<Link width="100%" href={notification.hyperlink} passHref>
-									<div>
-										<div
-											dangerouslySetInnerHTML={{ __html: notification.body_text }}>
-										</div>
-										<div className="timestamp">{formatTimeAgo(notification.timestamp)}</div>
-									</div>
-										<div className={notification.read_receipt ? "greenDot" : "redDot"}></div>
-								</Link>
-							</div>
-						))}
-					<div className="separator"></div>
-					<Link href="/account" passHref>
-						<div className="viewAllButton">View all</div>
-					</Link>
-				</div>
-			</>
-		)}
-	</>
+    <div ref={menuRef} className={`menu ${showMenu ? 'show' : ''}`}>
+      {notifications.length > 0 ? (
+        notifications.map((notification) => (
+          <div key={notification.id} className="notItem">
+            <Link width="100%" href={notification.hyperlink} passHref>
+              <div>
+<div style={{ lineHeight: '19px', marginBottom: '8px' }} dangerouslySetInnerHTML={{ __html: notification.preview_text }} />
+                <div className="timestamp">{formatTimeAgo(notification.timestamp)}</div>
+              </div>
+            </Link>
+						<div className="notIndicators">
+              <DeleteNotification onDelete={() => deleteNotification(notification.id)} />
+						</div>
+          </div>
+        ))
+      ) : (
+        <div className="noNotifications">No new notifications</div>
+      )}
 
+				<div className="separator"></div>
+				<Link href="/account" passHref>
+					<div className="viewAllButton">View all</div>
+				</Link>
+			</div>
+		</>
   );
 };
 
