42c0b19ae0ccb05c5261a427fbd2c1fdcb738ddf
blah148 <info@ploddings.com>
2025-08-04 13:36:00 -0600

    mt. zion about

--- pages/about.js
+++ pages/about.js
@@ -34,8 +34,8 @@ export default function About({ userId, ip }) {
     <div className="bodyA">
       <SEO
         title="About - Ploddings"
-        description="To persist your: (i) visit history, (ii) starred guitar tablature, and (iii) access the pitch-shifter and slow-downer, create an account on Ploddings with an email"
-        slug="/subscribe"
+        description="The back-story of Ploddings; a place to learn more about how to play pre-war blues & roots music, mainly for guitar players."
+        slug="/about"
       />
 			<Sidebar userId={userId} ip={ip} />
       <div className="mainFeedAll">
@@ -52,25 +52,16 @@ export default function About({ userId, ip }) {
 							<StabilizerText />
               <h1>About</h1>
 							<div>
-                <p>Ploddings (est. 2018) is a non-profit educational resource, focused on American early blues & folk styles for stringed-instruments.</p>
+                <p>Ploddings (est. 2018) is a not-for-profit music transcription & analysis project, focused on African-American early blues & folk styles for stringed-instruments.</p>
                 <h2>i) Partnership with Mt. Zion Memorial Fund</h2>
-                <p>Since 2023, Ploddings remainds proudly partnered with the Mt. Zion Memorial Fund, led by Dr. T. DeWayne Moore. However, in the interest of opening the transcription tablature to the public, membership fees at this time are not being collected. Instead, the Ploddings site at this time is testing whether it can be of more effective service in the long-run by prioritizing awareness over funds for blues preservation efforts, motivating this modification in direction. In this spirit, those who wish to donate to the Ploddings site are encouraged to instead donate directly to the Mt. Zion Memorial Fund through their website.</p>
+								<p>
+									Since May 2022, the Ploddings project has been proudly partnered with the Mt. Zion Memorial Fund (MZMF), led by Dr. T. DeWayne Moore, aiming to raise awareness of their blues preservation efforts. To paraphrase Quincy Jones, to the path ahead, it can help to recall the path behind. In that spirit, the Ploddings project aims to recall, and shed light on the artistry, of that preceding path. In noticing that the MZMF has demonstrated a like-minded sentiment, it helps to make the partnership feel natural.</p>
+                <p>In case the Ploddings site provides any learnings, similar to how a person might glean learnings by paying for guitar lessons locally or at a workshop, then please consider paying it forward by donating to the Mt. Zion Memorial Fund at their website: 
+									<a href="https://mtzionmemorialfund.com/product/donate_now/" target="_blank" rel="noopener"> here</a>;
+									the MZMF is a registered 501(c)(3) charity, per the IRS.
+								</p>
                 <p>The work of the Mt. Zion Memorial Fund includes: (i) the erection of tombstones for musicians such as Belton Sutherland, Bo Carter, Sonny Boy Williamson II, Mississippi Fred McDowell, Memphis Minnie, Sam Chatmon, Elmore James, Charley Patton, and Frank Stokes; (ii) the preservation of historical sites; and (iii) the creation of memorials such as the Mississippi John Hurt museum.</p>
-                <h3>How Payments/Donations are Processed</h3>
-                <p>During periods of raising funds (which is not the case at this time), 100% of membership fees on Ploddings are controlled by the Mt. Zion Memorial Fund organization as charitable donations*. To be specific, their bank account is attached to & directly receives all Stripe payments on Ploddings; therefore, no middle-man handling of payments/donations exists. </p>
-
-                <sub>
-	                <p>*For citizens of the USA, that means Ploddings membership fees were tax-deductible, as the Mt. Zion Memorial Fund has public charity status approved by the IRS per section 170(b)(1)(A)(vi) of the IRC.
-                  </p>
-                </sub>
-								<h2>ii) How Subscriptions Work</h2>
-								<p>Though memberships are not currently active, when they are active, users of Ploddings may sign-up for one-time or recurring fees, representing a donation to the Mt. Zion Memorial Fund account.</p>
-								<p>A membership grants privileges that include:</p>
-								<ol>
-									<li>Viewing & playback of transcription tablature, and</li>
-									<li>Usage of the slow-downer / pitch-shifter tool.</li>
-								</ol>
-                <h2>iii) More Information</h2>
+                <h2>ii) More Information</h2>
                 <p>In case questions remain, please reach out via the Ploddings <Link href="/contact">contact page</Link>. The custodian & builder of Ploddings is "blah"/Blah148. To learn more about the Mt. Zion Memorial Fund, its current endeavors, how to volunteer, or its relationship with Ploddings, please get in touch via <a href="https://mtzionmemorialfund.com/" target="_blank">their website</a>.</p>
 
 							</div>
