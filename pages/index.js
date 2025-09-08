bac27a3ca9ae95546238ab839701f03b4ea819bf
blah148 <info@ploddings.com>
2025-09-08 17:35:39 -0600

    about page & yt sub fix

--- pages/index.js
+++ pages/index.js
@@ -119,7 +119,7 @@ export default function Home({ userId, ip }) {
                 <svg role="img" height="22" width="22" aria-hidden="true" viewBox="0 0 24 24" data-encore-id="icon">
                   <path d="M13.5 1.515a3 3 0 0 0-3 0L3 5.845a2 2 0 0 0-1 1.732V21a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6h4v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V7.577a2 2 0 0 0-1-1.732l-7.5-4.33z"></path>
                 </svg>
-                <div className="homeText">Home</div>
+                <div className="homeText">Ploddings</div>
               </Link>
               <div style={{ display: 'flex', marginLeft: 'auto' }}>
                 <Menu userId={userId} />
